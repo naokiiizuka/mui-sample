cd0f89fd434c566ecceaabf1cecf5fe198bbb453
Naoki Iizuka <naokiiizuka@mac.com>
2024-02-18 01:45:00 +0900

    Add text-transform: none;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /* App.js */
 import * as React from "react";
 import { Button } from "@mui/material";
+/* ↓これ追加 */
+import styled from "@emotion/styled";
+
+/* ↓これ追加 */
+const TextButton = styled(Button)`
+  text-transform: none;
+`;
 
 const  App = () => {
   return (
     <>
-      <Button>text</Button>
+      {/* ↓タグを「TextButton」に変更 */}
+      <TextButton>text</TextButton>
       <Button variant="contained">contained</Button>
       <Button variant="outlined">outlined</Button>
     </>
